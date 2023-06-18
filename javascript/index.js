aa1c904be46808c4718986f7e0c3298eb8886419
abhishekGoyal274 <51013750+abhishekGoyal274@users.noreply.github.com>
2023-06-19 00:29:11 +0530

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -30,11 +30,3 @@ $(".change").on("click", function () {
   }
 });
 
-function scrollToTop() {
-  var position =
-      document.body.scrollTop || document.documentElement.scrollTop;
-  if (position) {
-      window.scrollBy(0, -Math.max(1, Math.floor(position / 10)));
-      scrollAnimation = setTimeout("scrollToTop()", 30);
-  } else clearTimeout(scrollAnimation);
-}
